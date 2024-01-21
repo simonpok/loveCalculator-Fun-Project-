24b903f5285969b652313bf2eee5fb0f31e2e840
simonpok <poksimon9@gmail.com>
2024-01-21 23:41:01 +0545

    Designing

--- tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-let textfield1 = document.getElementById('FirstPersonName');
-// let textfield2 = document.getElementById('SecondPersonName');
-// let submitButton = document.getElementById('GeneratePercentage');
