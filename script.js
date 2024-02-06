77efa7da89ccdbfabf35b36329b2e76016a0cbee
simonpok <poksimon9@gmail.com>
2024-02-06 14:33:51 +0545

    Changed fonts

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let submitButton = document.getElementById('GeneratePercentage');
 let FinalResult = document.getElementById('result');
 let RefreshPage = document.getElementById('clearField');
 // let iconPosition = document.getElementById('icon');
+FinalResult.classList.add('custom-text');
 
 let person1 = textfield1.value;
 let person2 = textfield2.value;
